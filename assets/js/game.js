d3e084a06af008a794abb0c859c8f96ed59191d5
Kingluddite <howley.phil@gmail.com>
2020-08-15 00:58:49 -0400

    Add way to attack multiple robots and break out of loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,43 +9,14 @@ let enemyHealth = 50;
 const enemyAttack = 12;
 
 const fight = function(enemyName) {
-        while (enemyHealth > 0) {
-                // console.log('Welcome to Robot Gladiators!');
-                console.log(enemyName);
+        while (enemyHealth > 0 && playerHealth > 0) {
                 const promptFight = window.prompt(
                         "Would you like to FIGHT or SKIP this battle? Enter 'f' or 's' to choose."
                 );
-
                 const lcPromptFight = promptFight.toLowerCase();
 
-                if (lcPromptFight === 'f') {
-                        // battle code here
-                        // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-                        enemyHealth -= playerAttack;
-                        console.log(
-                                `${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining`
-                        );
-
-                        // check enemy's health
-                        if (enemyHealth <= 0) {
-                                console.log(`${enemyName} has died!`);
-                        } else {
-                                console.log(`${enemyName} still has ${enemyHealth} health left`);
-                        }
-
-                        // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-                        playerHealth -= enemyAttack;
-                        console.log(
-                                `${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`
-                        );
-
-                        // check players's health
-                        if (playerHealth <= 0) {
-                                console.log(`${playerName} has died!`);
-                        } else {
-                                console.log(`${playerName} still has ${playerHealth} health left.`);
-                        }
-                } else if (lcPromptFight === 's') {
+                // if user picks "skip" confirm and then stop the loop
+                if (lcPromptFight === 's') {
                         // confirm user wants to skip
                         const confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -53,14 +24,45 @@ const fight = function(enemyName) {
                         if (confirmSkip) {
                                 console.log(`${playerName} has decided to skip this fight. Goodbye!`);
                                 // penalize for skipping
-                                playerMoney -= 2;
+                                playerMoney -= 10;
+                                console.log(`playerMoney, ${playerMoney}`);
+                                break;
                         }
-                        // if false, ask question again by running fight() again
-                        else {
-                                fight();
-                        }
+                }
+
+                // battle code here
+                // remove enemy's health by subtracting the amount set in the playerAttack variable
+                enemyHealth -= playerAttack;
+                console.log(
+                        `${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining`
+                );
+
+                // check enemy's health
+                if (enemyHealth <= 0) {
+                        console.log(`${enemyName} has died!`);
+
+                        // award player money for winning
+                        playerMoney += 20;
+
+                        // leave while() loop since enemy is dead
+                        break;
                 } else {
-                        console.log('Please pick a valid fight option');
+                        console.log(`${enemyName} still has ${enemyHealth} health left`);
+                }
+
+                // remove players's health by subtracting the amount set in the enemyAttack variable
+                playerHealth -= enemyAttack;
+                console.log(
+                        `${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`
+                );
+
+                // check players's health
+                if (playerHealth <= 0) {
+                        console.log(`${playerName} has died!`);
+                        // leave while() loop if player is dead
+                        break;
+                } else {
+                        console.log(`${playerName} still has ${playerHealth} health left.`);
                 }
         }
 };
