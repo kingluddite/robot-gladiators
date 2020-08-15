e688cf93b16b9f29843fbdc7532519bd5f277a16
Kingluddite <howley.phil@gmail.com>
2020-08-15 01:21:07 -0400

    add way for player to die and rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,7 +68,23 @@ const fight = function(enemyName) {
 };
 
 for (let i = 0; i < enemyNames.length; i += 1) {
-        const pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
+        if (playerHealth > 0) {
+                // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+                console.log(`Welcome to Robot Gladiators! Round (${i + 1})`);
+
+                // pick new enemy to fight based on the index of the enemyNames array
+                const pickedEnemyName = enemyNames[i];
+
+                // reset enemyHealth before starting new fight
+                enemyHealth = 50;
+
+                // use debugger to pause script from running and check what's going on at that moment in the code
+                // debugger;
+
+                // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+                fight(pickedEnemyName);
+        } else {
+                console.log('You have lost your robot battle! Game over!');
+                break;
+        }
 }
