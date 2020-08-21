95f6db7c7130dcd91220b1addc305222d01fda1d
Kingluddite <howley.phil@gmail.com>
2020-08-21 15:35:58 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,6 +64,51 @@ const fight = function(enemyName) {
   }
 };
 
+const startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (let i = 0; i < enemyNames.length; i += 1) {
+    if (playerHealth > 0) {
+      // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      console.log(`Welcome to Robot Gladiators! Round (${i + 1})`);
+
+      // pick new enemy to fight based on the index of the enemyNames array
+      const pickedEnemyName = enemyNames[i];
+
+      // reset enemyHealth before starting new fight
+      enemyHealth = 50;
+
+      // use debugger to pause script from running and check what's going on at that moment in the code
+      //       debugger;
+
+      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
+
+      // if we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        // ask if user wants to use the store before next round
+        const storeConfirm = window.confirm('The fight is over, visit the store before the next round?');
+
+        // if yes, take them to the store() function
+        if (storeConfirm) {
+          shop();
+        }
+      }
+    } else {
+      console.log('You have lost your robot battle! Game over!');
+      break;
+    }
+  }
+
+  // after the for loop ends, player is either out of health or enemies to fight, so run the endGame function
+  endGame();
+
+  // play again
+  //   startGame();
+};
+
 // function to end the entire game
 const endGame = function() {
   // if player is still alive, player wins!
@@ -85,39 +130,52 @@ const endGame = function() {
   }
 };
 
-const startGame = function() {
-  debugger;
-  // reset player stats
-  playerHealth = 100;
-  playerAttack = 10;
-  playerMoney = 10;
-  for (let i = 0; i < enemyNames.length; i += 1) {
-    if (playerHealth > 0) {
-      // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      console.log(`Welcome to Robot Gladiators! Round (${i + 1})`);
+const shop = function() {
+  const shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
 
-      // pick new enemy to fight based on the index of the enemyNames array
-      const pickedEnemyName = enemyNames[i];
+  // use switch to carry out action
+  switch (shopOptionPrompt) {
+    case 'REFILL':
+    case 'refill':
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
 
-      // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+        // increase health and decrease money
+        playerHealth += 20;
+        playerMoney -= 7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
 
-      // use debugger to pause script from running and check what's going on at that moment in the code
-      //       debugger;
-
-      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-      fight(pickedEnemyName);
-    } else {
-      console.log('You have lost your robot battle! Game over!');
       break;
-    }
+    case 'UPGRADE':
+    case 'upgrade':
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+        // increase attack and decrease money
+        playerAttack += 6;
+        playerMoney -= 7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
+
+      break;
+    case 'LEAVE':
+    case 'leave':
+      window.alert('Leaving the store.');
+
+      // do nothing, so function will end
+      break;
+    default:
+      window.alert('You did not pick a valid option. Try again.');
+
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
   }
-
-  // after the for loop ends, player is either out of health or enemies to fight, so run the endGame function
-  endGame();
-
-  // play again
-  //   startGame();
 };
 
 // start game when page loads
